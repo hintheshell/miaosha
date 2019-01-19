d8e1262a492317ad473ced1781839e30685012c7
spicery <jkq12345@gmail.com>
2019-01-19 12:47:58 +0800

    feat(core):redis

--- src/main/java/com/imooc/miaosha/controller/SampleController.java
+++ src/main/java/com/imooc/miaosha/controller/SampleController.java
@@ -2,6 +2,8 @@ package com.imooc.miaosha.controller;
 
 
 import com.imooc.miaosha.domain.User;
+import com.imooc.miaosha.redis.RedisService;
+import com.imooc.miaosha.redis.UserKey;
 import com.imooc.miaosha.result.Result;
 import com.imooc.miaosha.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SampleController {
     @Autowired
     UserService userService;
-
+    @Autowired
+    RedisService redisService;
 
     @RequestMapping("/themaleaf")
     public String themaleaf(Model model) {
@@ -23,11 +26,28 @@ public class SampleController {
         return "hello";
     }
 
+    //数据库测试
     @RequestMapping("/db/get")
     @ResponseBody
     public Result<User> dbGet() {
         User user = userService.getById(1);
         return Result.success(user);
     }
-    
+
+    @RequestMapping("/redis/set")
+    @ResponseBody
+    public Result<Boolean> redisSet() {
+        User user = new User();
+        user.setId(1);
+        user.setName("1111");
+        redisService.set(UserKey.getById, "" + 1, user);//UserKey:id1  !!这里有失效时间 默认30秒
+        return Result.success(true);
+    }
+
+    @RequestMapping("/redis/get")
+    @ResponseBody
+    public Result<User> getRedis() {
+        User user = redisService.get(UserKey.getById, "" + 1, User.class);
+        return Result.success(user);
+    }
 }
