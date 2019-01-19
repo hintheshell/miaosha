e85d9516392b6a2bcc0bf543740e7e547e13707b
spicery <jkq12345@gmail.com>
2019-01-19 11:55:10 +0800

    feat(core):mysql druid thymeleaf

---
+++ src/main/java/com/imooc/miaosha/controller/SampleController.java
@@ -0,0 +1,33 @@
+package com.imooc.miaosha.controller;
+
+
+import com.imooc.miaosha.domain.User;
+import com.imooc.miaosha.result.Result;
+import com.imooc.miaosha.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/demo")
+public class SampleController {
+    @Autowired
+    UserService userService;
+
+
+    @RequestMapping("/themaleaf")
+    public String themaleaf(Model model) {
+        model.addAttribute("name", "Joshua");
+        return "hello";
+    }
+
+    @RequestMapping("/db/get")
+    @ResponseBody
+    public Result<User> dbGet() {
+        User user = userService.getById(1);
+        return Result.success(user);
+    }
+    
+}
